28a52c13d8d14a4e2c26ada5abf725eac7fe9cd4
Robert <taylorr82@mymacewan.ca>
2021-05-31 13:57:36 -0600

    set up flash to mirror functioning of bootloader

--- source/HL_sys_link.cmd
+++ source/HL_sys_link.cmd
@@ -56,11 +56,12 @@ MEMORY
 {
 /* USER CODE BEGIN (2) */
 /* USER CODE END */
-    VECTORS (X)  : origin=0x00000000 length=0x00000020
-    FLASH0  (RX) : origin=0x00000020 length=0x001FFFE0
-    FLASH1  (RX) : origin=0x00200000 length=0x00200000
-    STACKS  (RW) : origin=0x08000000 length=0x00001500
-    RAM     (RW) : origin=0x08001500 length=0x0007eb00
+    VECTORS (X)    : origin=0x00000000 length=0x00000020
+    BOOTFLASH (RX) : origin=0x00000020 length=0x00018000 - 0x20
+    FLASH0  (RX)   : origin=0x00018000 length=0x00200000 - 0x00018000 - 0x20
+    FLASH1  (RX)   : origin=0x00200000 length=0x00200000
+    STACKS  (RW)   : origin=0x08000000 length=0x00001500
+    RAM     (RW)   : origin=0x08001500 length=0x0007eb00
 
 /* USER CODE BEGIN (3) */
 /* USER CODE END */
@@ -83,17 +84,17 @@ SECTIONS
         Fapi_UserDefinedFunctions.obj (.text)
         bl_flash.obj (.text)
         --library = ../lib/F021_API_CortexR4_BE_L2FMC_V3D16.lib (.text)
-    } load=FLASH0, run=RAM, LOAD_START(FlashApi_LoadStart), SIZE(FlashApi_LoadSize), RUN_START(FlashApi_RunStart)
-    .text   align(32) : {} > FLASH0
-    .const  align(32) : {} > FLASH0
-    .cinit  align(32) : {} > FLASH0
-    .pinit  align(32) : {} > FLASH0
+    } load=BOOTFLASH, run=RAM, LOAD_START(FlashApi_LoadStart), SIZE(FlashApi_LoadSize), RUN_START(FlashApi_RunStart)
+    .text   align(32) : {} > BOOTFLASH
+    .const  align(32) : {} > BOOTFLASH
+    .cinit  align(32) : {} > BOOTFLASH
+    .pinit  align(32) : {} > BOOTFLASH
     .bss     : {} > RAM
     .data    : {} > RAM
     .sysmem  : {} > RAM
 	
-    FEE_TEXT_SECTION  : {} > FLASH0
-    FEE_CONST_SECTION : {} > FLASH0
+    FEE_TEXT_SECTION  : {} > BOOTFLASH
+    FEE_CONST_SECTION : {} > BOOTFLASH
     FEE_DATA_SECTION  : {} > RAM
 
 /* USER CODE BEGIN (6) */
