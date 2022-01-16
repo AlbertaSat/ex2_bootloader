125916d2c882c117f43e0a3b7acf300cc8d77fd4
Robert Taylor <taylorr82@mymacewan.ca>
2022-01-15 23:03:06 -0700

    Split memory in to needed sections. Needs optimization

--- source/HL_sys_link.cmd
+++ source/HL_sys_link.cmd
@@ -48,18 +48,21 @@
 /* USER CODE END */
 
 /*----------------------------------------------------------------------------*/
+/* Misc                                                                       */
+
+/* USER CODE BEGIN (6) */
 /* Memory Map                                                                 */
 
 MEMORY
 {
-    VECTORS (X)      : origin=0x00000000 length=0x00000040
-    BOOTFLASH (RX)   : origin=0x00000040 length=0x00020000 - 0x40
-    FLASH0  (RX)     : origin=end(BOOTFLASH) length=0x00200000 - 0x00020000 - 0x20
-    FLASH1  (RX)     : origin=0x00200000 length=0x00200000
-    RAMINTVECS (RWX) : origin=0x08000000 length=0x00000020
-    KRAM  (RW)     : origin=end(RAMINTVECS) length=0x00000800
-    RAM     (RW)     : origin=end(KRAM) length=0x0007eb00 - 0x08
-    
+    VECTORS (X)  : origin=0x00000000 length=0x00000040
+    KERNEL  (RX) : origin=0x00000040 length=0x00008000
+    BOOTFLASH (RX) : origin=end(KERNEL) length = 0x00030000
+    FLASH0  (RX) : origin=end(BOOTFLASH) length=0x001F7FE0 - 0x00030000 - 0x00008000 - 0x00000040
+    FLASH1  (RX) : origin=0x00200000 length=0x00200000
+    RAMINTVECS (RWX) : origin=0x08000000 length = 0x20
+    KRAM    (RW)     : origin=end(RAMINTVECS) length=0x00000800
+    RAM     (RW)     : origin=end(KRAM)   length=0x0007FFFF - 0x800 - 0x20
 /* USER CODE BEGIN (2) */
 /* USER CODE END */
 }
@@ -74,9 +77,9 @@ SECTIONS
 {
     .intvecs : {} > VECTORS
     /* FreeRTOS Kernel in protected region of Flash */
-    .kernelTEXT  align(32) : {} > BOOTFLASH
-    .cinit       align(32) : {} > BOOTFLASH
-    .pinit       align(32) : {} > BOOTFLASH
+    .kernelTEXT  align(32) : {} > KERNEL
+    .cinit       align(32) : {} > KERNEL
+    .pinit       align(32) : {} > KERNEL
     /* Rest of code to user mode flash region */
     .text        align(32) : {} > BOOTFLASH
     .const       align(32) : {} > BOOTFLASH
@@ -84,13 +87,12 @@ SECTIONS
     .kernelBSS    : {} > KRAM
     .kernelHEAP   : {} > RAM
     .bss          : {} > RAM
-    .data         : {} > RAM    
+    .data         : {} > RAM
     .sysmem       : {} > RAM
     FEE_TEXT_SECTION align(32) : {} > BOOTFLASH
     FEE_CONST_SECTION align(32): {} > BOOTFLASH
     FEE_DATA_SECTION : {} > RAM
 
-/* USER CODE BEGIN (4) */
 	flashAPI :
 	    {
 	        Fapi_UserDefinedFunctions.obj (.text)
@@ -98,16 +100,7 @@ SECTIONS
 	        --library = ../lib/F021_API_CortexR4_BE_L2FMC_V3D16.lib (.text)
 	    } load=BOOTFLASH, run=RAM, LOAD_START(FlashApi_LoadStart), SIZE(FlashApi_LoadSize), RUN_START(FlashApi_RunStart)
 	.ramIntvecs : {} load=BOOTFLASH, run=RAMINTVECS, palign=8, LOAD_START(ramint_LoadStart), SIZE(ramint_LoadSize), RUN_START(ramint_RunStart)
-/* USER CODE END */
 }
-
-/* USER CODE BEGIN (5) */
-/* USER CODE END */
-
-/*----------------------------------------------------------------------------*/
-/* Misc                                                                       */
-
-/* USER CODE BEGIN (6) */
 /* USER CODE END */
 
 /*----------------------------------------------------------------------------*/
