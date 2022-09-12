c4b47d8fa4413f992b8fe796efc854600bc2a93c
Robert Taylor <taylorr@ualberta.ca>
2022-09-11 21:24:49 -0600

    Attempt to copy golden image in to application image
    This reduces complexity because it is not longer necessary to do special golden image linkage, the same release binary can be used as a golden image and application image
    If the application image fails verification it will be erased anyway, so the golden image may as well take its place
    The golden image can still be updated in flight.
    If the golden image fails verification the bootloader will still run

--- bl_source/bl_launch.c
+++ bl_source/bl_launch.c
@@ -20,11 +20,36 @@ bool start_application(void) {
     return false;
 }
 
+bool copy_golden_image() {
+    image_info golden_info = {0};
+    eeprom_get_golden_info(&golden_info);
+    if (!verify_golden()) {
+        return false;
+    }
+    image_info app_info = {0};
+    memcpy(&app_info, &golden_info, sizeof(image_info));
+    app_info.addr = APP_MINIMUM_ADDR; // This is an emergency, so this will do
+
+    eeprom_set_app_info(&app_info);
+
+    if (Fapi_BlockErase(app_info.addr, app_info.size) != 0) {
+        return false;
+    }
+
+    uint8_t bank = 1; // This emergency will always write to bank 1, since that's the minimum app addr
+                      // Also, if it tries for some reason to write to bank 0 and an interrupt occurs,
+                      // a prefetch abort will happen.
+    if (Fapi_BlockProgram(bank, app_info.addr, golden_info.addr, app_info.size) != 0) {
+        return false;
+    }
+    return true;
+}
+
 void start_golden() {
     if (verify_golden() == true) {
-        image_info app_info = {0};
-        eeprom_get_golden_info(&app_info);
-        uint32_t JumpAddress = (uint32_t)app_info.addr;
-        ((void (*)(void))JumpAddress)();
+        if (copy_golden_image() == false) {
+            return;
+        }
+        sw_reset("A", REQUESTED);
     }
 }
